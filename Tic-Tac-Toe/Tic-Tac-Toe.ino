0f62baeb431ca7140963a3ca0056174ebbdb124a
AlphaPanda <dominggold@gmail.com>
2021-05-05 13:37:45 +0200

    warum liegt hier stroh - Bild

--- Tic-Tac-Toe/Tic-Tac-Toe.ino
+++ Tic-Tac-Toe/Tic-Tac-Toe.ino
@@ -1,3 +1,20 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>      // Hardware-specific library
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+#include "logo.h"
+
+void setup(void)
+{
+    pinMode(0, INPUT);
+    pinMode(35, INPUT);
+    tft.init();
+    tft.setRotation(0);
+
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0, 135, 240, bootlogo);
+}
+
+void loop(void)
+{
+    
+}
