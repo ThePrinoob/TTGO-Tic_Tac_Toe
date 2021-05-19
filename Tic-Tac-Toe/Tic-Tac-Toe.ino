d9e956d02480c2bb22d28024bbbfb009f5261e3c
AlphaPanda <dominggold@gmail.com>
2021-05-19 12:44:42 +0200

    Draw Playground and defined positons

--- Tic-Tac-Toe/Tic-Tac-Toe.ino
+++ Tic-Tac-Toe/Tic-Tac-Toe.ino
@@ -22,6 +22,7 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
 void setup(void)
 {
+    Serial.begin(9600);
     pinMode(0, INPUT);
     pinMode(35, INPUT);
     tft.init();
@@ -50,9 +51,43 @@ void setup(void)
     // tft.pushImage(10, 50, 16, 16, cross);
     // tft.pushImage(30, 30, 16, 16, circle);
     tft.drawCircle(30, 30, 16, BLACK);
+
+    // horizontal - 
+    tft.drawLine(10, 85, 125, 85, BLACK); // first line
+    tft.drawLine(10, 155, 125, 155, BLACK); // second line
+
+    // Vertikal |
+    tft.drawLine(45, 30, 45, 210, BLACK); // first line
+    tft.drawLine(90, 30, 90, 210, BLACK); // second line
+
+    // length of the quadrat in which the x is placed
+    int lengthX = 25;
+    // Positions from left to right top to bottom
+    int positionsXY[9][9] = { 
+        {15, 50},
+        {15, 110},
+        {15, 170},
+        {55, 50},
+        {55, 110},
+        {55, 170},
+        {95, 50},
+        {95, 110},
+        {95, 170}
+    };
+
+    // loop through the positions array - sizeof needs to be done two times because its a multidimensional array.
+    for ( int i = 0; i < sizeof(positionsXY) / sizeof (positionsXY[0]); ++i ) {
+        drawX(positionsXY[i][0], positionsXY[i][1], lengthX, BLACK);
+    }
 }
 
 void loop(void)
 {
 
+}
+
+int drawX(int x, int y, int l, uint32_t color){
+    tft.drawLine(x, y, x+l, y+l, color);
+    tft.drawLine(x, y+l, x+l, y, color);
+    return 0;
 }
