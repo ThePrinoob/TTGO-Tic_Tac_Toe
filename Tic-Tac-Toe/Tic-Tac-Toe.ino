24acdab582fb2280d62a6d05daaa313ece8cc04d
ThePrinoob <dominggold@gmail.com>
2021-05-24 19:03:23 +0200

    comments

--- Tic-Tac-Toe/Tic-Tac-Toe.ino
+++ Tic-Tac-Toe/Tic-Tac-Toe.ino
@@ -5,9 +5,11 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 #include "cross.h"         // Import cross picture
 #include "circle.h"        // Import circle picture
 
+// Colors
 #define post_yellow 0xFE60
 #define BLACK   0x0000
 
+// Scene
 int scene = 0;
 
 // Positions from left to right top to bottom
@@ -23,6 +25,7 @@ int positionsXY[9][2] = {
     {100, 170}
 };
 
+// current position
 int position = -1;
 
                     //  x | x | x
@@ -40,19 +43,24 @@ int quantity = sizeof(positionsXY) / sizeof (positionsXY[0]);
 // length of the quadrat in which the x is placed
 int lengthX = 25;
 
+// current player
 int player = 1;
 
 void setup(void)
 {
+    // initialize Serial Output
     Serial.begin(115200);
     
+    // Set pin mode of the two inbuilt buttons
     pinMode(0, INPUT);
     pinMode(35, INPUT);
 
+    // Init TFT
     tft.init();
     tft.setRotation(0);
 
     tft.setSwapBytes(true);
+
     // tft.pushImage(0, 0, 135, 240, bootlogo);
 
     // tft.setTextColor(TFT_WHITE, TFT_BLACK);
@@ -61,35 +69,17 @@ void setup(void)
     // tft.setCursor(0, 0, 2);
     // tft.println("Warum liegt hier stroh");
 
-    
-    // tft.drawLine(0, 17, 0, 240, post_yellow);
-    // tft.drawLine(0, 17, 135, 17, TFT_GREY);
-    // tft.drawLine(134, 17, 134, 240, TFT_GREY);
-
     // Draw yellow screen
     tft.fillScreen(post_yellow);
-  
-    // //Draw yellow frame
-    // tft.drawRect(0,0,135,240,BLACK);
 
-    // // tft.pushImage(10, 50, 16, 16, cross);
-    // // tft.pushImage(30, 30, 16, 16, circle);
-    // tft.drawCircle(30, 30, 16, BLACK);
-
-    // horizontal - 
+    // draw horizontal lines - 
     tft.drawLine(10, 85, 125, 85, BLACK); // first line
     tft.drawLine(10, 155, 125, 155, BLACK); // second line
 
-    // Vertikal |
+    // draw vertical lines |
     tft.drawLine(45, 30, 45, 210, BLACK); // first line
     tft.drawLine(90, 30, 90, 210, BLACK); // second line
 
-
-
-    // // loop through the positions array - sizeof needs to be done two times because its a multidimensional array.
-    // for ( int i = 0; i < sizeof(positionsXY) / sizeof (positionsXY[0]); ++i ) {
-    //     drawX(positionsXY[i][0], positionsXY[i][1], lengthX, BLACK);
-    // }
 }
 
 void loop()
@@ -101,6 +91,7 @@ void loop()
         positionsSet[position] = player;
 
         // When all moves are made go to next scene
+        // Will be replaced, because it needs to be checked if a player already won.
         for ( int i = 0; i < quantity; ++i ) {
             if (positionsSet[i] == 0) {
                 break;
@@ -111,6 +102,7 @@ void loop()
                 }
             }
         }
+
         // reset the position
         position = -1;
 
@@ -132,32 +124,31 @@ void loop()
         Serial.println(position);
         Serial.println(player);
 
-        buttonRight();
+        position++;
+
+        determinePosition();
+
+        // draw empty space
+        for ( int i = 0; i < sizeof(positionsXY) / sizeof (positionsXY[0]); ++i ) {
+            if (positionsSet[i] == 0) {
+                tft.fillRect(positionsXY[i][0] - 8, positionsXY[i][1] - 5, lengthX + 12, lengthX + 10, post_yellow);
+            }
+        }
+
+        drawPlayer();
 
         delay(250);
     }
 }
 
-void buttonRight() {
-    
-    position++;
-    determinePosition();
-
-    // draw empty space - could be solved better
-    for ( int i = 0; i < sizeof(positionsXY) / sizeof (positionsXY[0]); ++i ) {
-        if (positionsSet[i] == 0) {
-            tft.fillRect(positionsXY[i][0] - 8, positionsXY[i][1] - 5, lengthX + 12, lengthX + 10, post_yellow);
-        }
-    }
-
-    drawPlayer();
-}
-
+// This function determines the next possible position a player can make
+// return void
 void determinePosition() {
     // change the position only if its not out of range
     if (position < quantity) {
         // check if the place is already taken.
         for (int i = 0; i < quantity; i++) {
+            // If the next field is also taken it starts the function again.
             if (positionsSet[position] != 0) {
                 position++;
                 determinePosition();
@@ -167,6 +158,7 @@ void determinePosition() {
             }
         }
     } else {
+        // if its out of range it sets the position to zero and searches the first possible move.
         position = 0;
         for (int i = 0; i < quantity; i++) {
             if (positionsSet[position] != 0) {
@@ -179,6 +171,8 @@ void determinePosition() {
     }
 }
 
+// Draws the current player at the current position (int) based on the player (int) variable.
+// returns void
 void drawPlayer() {
     if (player == 1) {
         Serial.println("player 1");
