b6dd6ef089418a3d37eb2cdcc6b91b43253862a0
ThePrinoob <dominggold@gmail.com>
2021-06-09 22:23:03 +0200

    Working communcation over wifi, winning not working

--- Tic-Tac-Toe/Tic-Tac-Toe.ino
+++ Tic-Tac-Toe/Tic-Tac-Toe.ino
@@ -1,6 +1,10 @@
+// #include <EEPROM.h> // Write Data to storage
 #include <SPI.h>
 #include <TFT_eSPI.h>      // Hardware-specific library
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+#include "esp_wifi.h"
+#include "ESPAsyncWebServer.h"
+#include <HTTPClient.h>
 
 // Colors
 #define post_yellow 0xFE60
@@ -8,9 +12,15 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 #define BLUE 0x001F
 #define RED 0xF800
 #define GREY 0x8C51
+#define WHITE 0xFFFF
+
+long colors[] = {0xFE60, 0x0000, 0x001F, 0xF800, 0x8C51}; // post_yellow, Black, Blue, Red, Grey
 
 // Scene
-int fase = 1;
+int fase = 0;
+
+// position of arrow on main screen
+int arrowPosition = 1;
 
 // Positions from left to right top to bottom
 int positionsXY[9][2] = {
@@ -59,10 +69,32 @@ unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 500; //the value is a number of milliseconds used for the blinking player
 
+// int storedColor;
+
+bool firstRun = true;
+
+// Set your access point network credentials
+const char* ssid = "ESP32-TicTacToe";
+const char* password = "ticTacToe";
+
+const char* serverGameGetPlayer = "http://192.168.4.1/gameGetPlayer";
+const char* serverGameGetPositions = "http://192.168.4.1/gameGetPositions";
+const char* serverGameSetPlayer = "http://192.168.4.1/gamePostPlayer";
+const char* serverGameSetPositions = "http://192.168.4.1/gamePostPositions";
+bool isServer = true;
+bool isOnline = true;
+
+AsyncWebServer server(80);
+
 void setup(void)
 {
+    // EEPROM.begin(10);
     // initialize Serial Output
     Serial.begin(115200);
+
+    // storedColor = EEPROM.read(0);
+
+    // Serial.println("Value: " + String(storedColor));
 
     // Set pin mode of the two inbuilt buttons
     pinMode(0, INPUT);
@@ -74,42 +106,147 @@ void setup(void)
 
     tft.setSwapBytes(true);
 
+    initScreen();
+
     // tft.pushImage(0, 0, 135, 240, bootlogo);
 
-    // Draw yellow screen
-    tft.fillScreen(post_yellow);
-
-    // draw horizontal lines -
-    tft.drawLine(10, 85, 125, 85, BLACK);   // first line
-    tft.drawLine(10, 155, 125, 155, BLACK); // second line
-
-    // draw vertical lines |
-    tft.drawLine(45, 30, 45, 210, BLACK); // first line
-    tft.drawLine(90, 30, 90, 210, BLACK); // second line
-
-    // Set color, text size and print the current player
-    // Text color needs BG, else it doesn't display the number correctly.
-    tft.setTextColor(BLACK, post_yellow);
-    tft.setTextSize(1);
-    tft.setCursor(5, 2, 2);
-    tft.println("Spieler: " + String(player));
-
     startMillis = millis(); //initial start time
+
+    // EEPROM.write(0, storedColor);
+
+    // EEPROM.commit();
+
+    // setupServer();
+
 }
 
 void loop()
 {
-
-    //start screen - TODO
+    //start screen
     if (fase == 0)
     {
-        delay(1000);
-        tft.fillScreen(0x0000);
+        if (digitalRead(0) == 0)
+        {
+            tft.fillScreen(TFT_BLACK);
+
+            tft.setCursor(5, 103, 2);
+
+            tft.setTextColor(TFT_WHITE, TFT_BLACK);
+            if (arrowPosition == 1)
+            {
+                isOnline = true;
+                isServer = true;
+                setupServer();
+            }
+            else if (arrowPosition == 2)
+            {
+                isOnline = true;
+                isServer = false;
+                setupClient();
+            }
+            else if (arrowPosition == 3)
+            {
+                isOnline = false;
+            }
+            fase += 1;
+        }
+
+        if (digitalRead(35) == 0)
+        { // button right
+            if (arrowPosition < 3)
+            {
+                int position = (52 * 1) * arrowPosition;
+                int newPosition = (52 * 1) * (arrowPosition + 1);
+
+                tft.setCursor(10, position, 1);
+                tft.println("  ");
+
+                tft.setCursor(10, newPosition, 1);
+                tft.println("->");
+
+                arrowPosition++;
+            }
+            else
+            {
+                int position = (52 * 1) * arrowPosition;
+                arrowPosition = 1;
+                int newPosition = (52 * 1) * (arrowPosition);
+
+                tft.setCursor(10, position, 1);
+                tft.println("  ");
+
+                tft.setCursor(10, newPosition, 1);
+                tft.println("->");
+            }
+            delay(250);
+        }
     }
 
     // game screen
     else if (fase == 1)
     {
+        currentMillis = millis(); //get the current "time" (actually the number of milliseconds since the program started)
+
+        if(firstRun) {
+            // Draw yellow screen
+            tft.fillScreen(post_yellow);
+
+            // draw horizontal lines -
+            tft.drawLine(10, 85, 125, 85, BLACK);   // first line
+            tft.drawLine(10, 155, 125, 155, BLACK); // second line
+
+            // draw vertical lines |
+            tft.drawLine(45, 30, 45, 210, BLACK); // first line
+            tft.drawLine(90, 30, 90, 210, BLACK); // second line
+
+            // Set color, text size and print the current player
+            // Text color needs BG, else it doesn't display the number correctly.
+            tft.setTextColor(BLACK, post_yellow);
+            tft.setTextSize(1);
+            tft.setCursor(5, 2, 2);
+            tft.println("Spieler: " + String(player));
+            firstRun = false;
+            delay(1000);
+        }
+
+        if (currentMillis - startMillis >= period) //test whether the period has elapsed
+        {
+            if (isOnline) {
+                if (!isServer) {
+                    player = httpGETRequest(serverGameGetPlayer).toInt();
+                    if (player == 2) {
+                        convertToArray(httpGETRequest(serverGameGetPositions));
+                        drawAllPlayers();
+                    } else {
+                        return;
+                    }
+                }
+            }
+
+            checkForWinner();
+
+            if (!isServer && player == 2 || isServer && player == 1) {
+                if (blink == 0)
+                {
+                    drawEmptySpace();
+                    blink = 1;
+                }
+                else
+                {
+                    determinePosition();
+                    drawPlayer();
+                    blink = 0;
+                }
+            }
+
+            startMillis = currentMillis; // save the start time of the current state.
+        }
+        if (isOnline) {
+            if (!isServer && player == 1 || isServer && player == 2) {
+                return;
+            }
+        }
+        
         if (digitalRead(0) == 0)
         { // button left
             Serial.println("Left Button");
@@ -119,30 +256,7 @@ void loop()
             // set the move of the player
             positionsSet[position] = player;
 
-            // Check every possible move and set the winner
-            score = positionsSet[0] + positionsSet[1] + positionsSet[2];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[3] + positionsSet[4] + positionsSet[5];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[6] + positionsSet[7] + positionsSet[8];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[0] + positionsSet[3] + positionsSet[6];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[1] + positionsSet[4] + positionsSet[7];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[2] + positionsSet[5] + positionsSet[8];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[0] + positionsSet[4] + positionsSet[8];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
-
-            score = positionsSet[2] + positionsSet[4] + positionsSet[6];
-            if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+            checkForWinner();
 
             // check if somebody won
             if (!winner)
@@ -157,43 +271,38 @@ void loop()
                         {
                             Serial.println(i);
                             Serial.println(quantity);
+                            player = (player == 1) ? 2 : 1;
                             fase++;
+                            firstRun = true;
                             winner = 3;
-                            nextScene(fase);
                         }
                     }
                 }
-
-                // reset the position
-                position = 0;
-
-                // change the player
-                if (player == 1)
-                {
-                    player = 2;
-                }
-                else
-                {
-                    player = 1;
-                }
-
-                // Display current player
-                tft.setCursor(5, 2, 2);
-                tft.println("Spieler: " + String(player));
-
-                determinePosition();
-
-                drawPlayer();
-
-                drawEmptySpace();
-
-                delay(250);
             }
-            else
-            {
-                fase++;
-                nextScene(fase);
+
+            // reset the position
+            position = 0;
+
+            // change the player
+            player = (player == 1) ? 2 : 1;
+
+            // Display current player
+            tft.setCursor(5, 2, 2);
+            tft.println("Spieler: " + String(player));
+
+            determinePosition();
+
+            drawPlayer();
+
+            drawEmptySpace();
+
+            if (!isServer && isOnline) {
+                httpPOSTRequest(serverGameSetPlayer, String(player));
+                String tmpPositionsSet = convertToString();
+                httpPOSTRequest(serverGameSetPositions, tmpPositionsSet);
             }
+
+            delay(250);
         }
 
         if (digitalRead(35) == 0)
@@ -211,30 +320,55 @@ void loop()
 
             delay(250);
         }
-
-        currentMillis = millis(); //get the current "time" (actually the number of milliseconds since the program started)
-
-        if (currentMillis - startMillis >= period) //test whether the period has elapsed
-        {
-            if (blink == 0)
-            {
-                drawEmptySpace();
-                blink = 1;
-            }
-            else
-            {
-                determinePosition();
-                drawPlayer();
-                blink = 0;
-            }
-            startMillis = currentMillis; // save the start time of the current state.
-        }
     }
     else if (fase == 2)
     {
-        //to do
-        setEndScreen();
+        if (firstRun) {
+            tft.fillScreen(TFT_BLACK);
+
+            tft.setCursor(13, 103, 2);
+
+            tft.setTextColor(TFT_WHITE, TFT_BLACK);
+            tft.setTextSize(1);
+
+            if (winner == 1)
+            {
+                tft.println("WINNER X");
+            }
+            else if (winner == 2)
+            {
+                tft.println("WINNER O");
+            }
+            else if (winner == 3)
+            {
+                tft.println("DRAW");
+            }
+            firstRun = false;
+        }
     }
+    else if (fase == 3)
+    {
+
+    }
+}
+
+void initScreen() {
+    tft.fillScreen(BLACK);
+    tft.setTextColor(WHITE, BLACK);
+    
+    tft.setTextSize(1);
+    
+    tft.setCursor(35, 50, 2);
+    tft.println("Host Game");
+
+    tft.setCursor(35, 100, 2);
+    tft.println("Join Game");
+    
+    tft.setCursor(35, 150, 2);
+    tft.println("Local Game");
+
+    tft.setCursor(10, 52, 1);
+    tft.println("->");
 }
 
 void drawEmptySpace()
@@ -305,6 +439,60 @@ void drawPlayer()
     }
 }
 
+void drawAllPlayers() {
+    drawEmptySpace();
+    for (int i = 0; i <= quantity; i++)
+    {
+        if (positionsSet[i] == 1)
+        {
+            drawX(positionsXY[i][0], positionsXY[i][1], lengthX, BLACK);
+        }
+        if (positionsSet[i] == 2)
+        {
+            tft.drawCircle(positionsXY[i][0] + 12, positionsXY[i][1] + 10, 14, BLACK);
+        }
+    }
+    determinePosition();
+    checkForWinner();
+    // Display current player
+    tft.setCursor(5, 2, 2);
+    tft.println("Spieler: " + String(player));
+}
+
+void checkForWinner() {
+    // Check every possible move and set the winner
+    score = positionsSet[0] + positionsSet[1] + positionsSet[2];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[3] + positionsSet[4] + positionsSet[5];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[6] + positionsSet[7] + positionsSet[8];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[0] + positionsSet[3] + positionsSet[6];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[1] + positionsSet[4] + positionsSet[7];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[2] + positionsSet[5] + positionsSet[8];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[0] + positionsSet[4] + positionsSet[8];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    score = positionsSet[2] + positionsSet[4] + positionsSet[6];
+    if(score == 3 || score == 6) { winner = (score == 3) ? 1 : 2; }
+
+    if (winner) {
+        // Change the player for the last time so it can be grabt by the client.
+        player = (player == 1) ? 2 : 1;
+        fase++;
+        firstRun = true;
+    }
+}
+
 // Draws a X with the given coordinates and color
 void drawX(int x, int y, int l, uint32_t color)
 {
@@ -312,25 +500,146 @@ void drawX(int x, int y, int l, uint32_t color)
     tft.drawLine(x, y + l, x + l, y, color);
 }
 
-// TBD
-void nextScene(int sceneNumber)
-{
-    tft.fillScreen(BLACK);
+// Converts the post Request back to the Array.
+void convertToArray(String tempPositionsSet) {
+    int str_len = tempPositionsSet.length() + 1; 
+    char char_array[str_len];
+    tempPositionsSet.toCharArray(char_array, str_len);
+
+    const char* ptr = strtok(char_array, ",");
+    byte i = 0;
+    while (ptr) {
+        positionsSet[i] = atol(ptr);
+        ptr = strtok(NULL, ",");
+        i++;
+    }
 }
 
-// Set the end screen with the winner variabel.
-void setEndScreen()
-{
-    if (winner == 1)
+String convertToString() {
+    String response = "";
+    for (int i = 0; i <= quantity; i++)
     {
-        tft.fillScreen(BLUE);
+        if (i != quantity)
+            response = response + String(positionsSet[i]) + ",";
+        else
+            response = response + String(positionsSet[i]);
     }
-    else if (winner == 2)
+    return response;
+}
+
+void setupServer() {
+
+    // Setting the ESP as an access point
+    Serial.print("Setting AP (Access Point)…");
+
+    WiFi.softAP(ssid, password, 1, 1, 1); // ssid, password, channel, hidden SSID, max Connections
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    tft.println("Waiting for Player..");
+
+    while (WiFi.softAPgetStationNum() != 1) {
+        delay(100);
+    }
+
+    server.on("/gameGetPositions", HTTP_GET, [](AsyncWebServerRequest *request)
     {
-        tft.fillScreen(RED);
+        String response = convertToString();
+        request->send(200, "text/plain", response);
+    });
+
+    server.on("/gameGetPlayer", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", String(player));
+    });
+
+    server.on("/gamePostPositions", HTTP_POST, [](AsyncWebServerRequest *request){
+        AsyncWebParameter *p = request->getParam(0);
+        String tempPositionsSet = p->value().c_str();
+
+        Serial.println("POST Positions: " + tempPositionsSet);
+
+        convertToArray(tempPositionsSet);
+        drawAllPlayers();
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.on("/gamePostPlayer", HTTP_POST, [](AsyncWebServerRequest *request){
+        AsyncWebParameter *p = request->getParam(0);
+        const char* tmpPlayer = p->value().c_str();
+
+        player = String(tmpPlayer).toInt();
+        Serial.println("POST PLAYER: " + String(player));
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.begin();
+}
+
+void setupClient() {    
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+    tft.println("Connecting..");
+    while(WiFi.status() != WL_CONNECTED) { 
+        delay(100);
+        Serial.print(".");
     }
-    else if (winner == 3)
-    {
-        tft.fillScreen(GREY);
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+String httpGETRequest(const char* serverName) {
+    HTTPClient http;
+        
+    // Your IP address with path or Domain name with URL path 
+    http.begin(serverName);
+    
+    // Send HTTP GET request
+    int httpResponseCode = http.GET();
+    
+    String payload = "--"; 
+    
+    if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString();
+    } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
     }
+    // Free resources
+    http.end();
+
+    return payload;
+}
+
+String httpPOSTRequest(const char* serverName, String payload) {
+    HTTPClient http; 
+    
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String preparedPayload = "test=" + payload;
+    Serial.println(preparedPayload);
+    int httpResponseCode = http.POST(preparedPayload);
+
+    String response = "--"; 
+    
+    if(httpResponseCode>0){
+    
+        response = http.getString();    
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+    
+    } else{
+    
+        Serial.print("Error on sending POST Request: ");
+        Serial.println(httpResponseCode);
+    
+    }
+    
+    http.end();
+    return response;
 }
